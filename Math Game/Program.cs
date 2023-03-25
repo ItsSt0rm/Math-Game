6b4bcd8127b68b34c27ef87141f10c89064b1c06
Storm <esteban.palaciov@hotmail.com>
2023-03-24 23:04:42 -0500

    Read input and validate options

--- Math Game/Program.cs
+++ Math Game/Program.cs
@@ -1,6 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Testing Git again"); Console.WriteLine("Testing Git again");
-Console.WriteLine("Testing Git again");
-Console.WriteLine("Testing Git again");
-Console.WriteLine("Testing Git again");
+﻿List<int> gamesHistory = new List<int>();
 
+Console.WriteLine("Welcome to the Math Game, please write the number of the option that you want to choose");
+Console.WriteLine($"1. Addition" +
+                  $"\n2. Substraction" +
+                  $"\n3. Multiplication" +
+                  $"\n4. Division" +
+                  $"\n0. Exit\n");
+
+string line;
+while(!String.IsNullOrEmpty(line = Console.ReadLine()))
+{
+    int selectedOption = int.Parse(line);
+
+    if(selectedOption < 0 || selectedOption > 4)
+    {
+        Console.WriteLine(" ----------- You must choose a valid option -----------");
+    }
+
+    if(selectedOption == 0)
+    {
+        break;
+    }
+
+}
+
+Console.WriteLine("----------- Thanks for playing -----------");
