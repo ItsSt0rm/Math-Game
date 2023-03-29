77bbf664178fffd9f862678f9d0d871f47fc8fbb
Storm <esteban.palaciov@hotmail.com>
2023-03-29 16:15:26 -0500

    Operations implemented

--- Math Game/Program.cs
+++ Math Game/Program.cs
@@ -4,7 +4,8 @@ string menuOptions = "1. Addition" +
                   "\n2. Substraction" +
                   "\n3. Multiplication" +
                   "\n4. Division" +
-                  "\n5. Games history" +
+                  "\n5. Previous games" +
+                  "\n6. Change difficult (Normal by default)" +
                   "\n0. Exit\n";
 string line;
 List<string> gamesHistory = new List<string>();
@@ -17,9 +18,9 @@ while (!String.IsNullOrEmpty(line = Console.ReadLine()))
 {
     int selectedOption = int.Parse(line);
     int correctAnswers;
-    int numberQuestions = 2;
+    int numberQuestions = 4;
 
-    if (selectedOption < 0 || selectedOption > 4)
+    if (selectedOption < 0 || selectedOption > 5)
     {
         Console.WriteLine("----------- You must choose a valid option -----------");
     }
@@ -29,23 +30,75 @@ while (!String.IsNullOrEmpty(line = Console.ReadLine()))
         break;
     }
 
+    if (selectedOption == 5)
+    {
+        Console.WriteLine("----------- Previous Games -----------\n");
+
+        foreach (var item in gamesHistory)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
     if (selectedOption > 0 && selectedOption < 5)
     {
         Console.WriteLine($"----------- Game {game} -----------");
         correctAnswers = 0;
 
-        for (int i = 0; i < numberQuestions && correctAnswers == i; i++)
+        for (int i = 0; i < numberQuestions && i == correctAnswers; i++)
         {
-            int firstNumber = new Random().Next(101);
-            int secondNumber = new Random().Next(101);
+            int firstNumber = new Random().Next(1, 10);
+            int secondNumber = new Random().Next(1, 10);
 
             switch (selectedOption)
             {
                 case 1:
                     Console.WriteLine($"{firstNumber} + {secondNumber} = ?");
-                    int.TryParse(Console.ReadLine(), out int response);
+                    int.TryParse(Console.ReadLine(), out int responseAddition);
 
-                    if (response == firstNumber + secondNumber) 
+                    if (responseAddition == firstNumber + secondNumber) 
+                    {
+                        correctAnswers++;
+                        Console.WriteLine("----------- Correct! -----------");
+                    }
+
+                    break;
+
+                case 2:
+                    Console.WriteLine($"{firstNumber} - {secondNumber} = ?");
+                    int.TryParse(Console.ReadLine(), out int responseSubstraction);
+
+                    if (responseSubstraction == firstNumber - secondNumber)
+                    {
+                        correctAnswers++;
+                        Console.WriteLine("----------- Correct! -----------");
+                    }
+
+                    break;
+
+                case 3:
+                    Console.WriteLine($"{firstNumber} * {secondNumber} = ?");
+                    int.TryParse(Console.ReadLine(), out int responseMultiplication);
+
+                    if (responseMultiplication == firstNumber * secondNumber)
+                    {
+                        correctAnswers++;
+                        Console.WriteLine("----------- Correct! -----------");
+                    }
+
+                    break;
+
+                case 4:
+                    while (firstNumber % secondNumber != 0 || secondNumber == 0)
+                    {
+                        firstNumber = new Random().Next(20);
+                        secondNumber = new Random().Next(1, 101);
+                    }
+
+                    Console.WriteLine($"{firstNumber} / {secondNumber} = ?");
+                    int.TryParse(Console.ReadLine(), out int responseDivision);
+
+                    if (responseDivision == firstNumber / secondNumber)
                     {
                         correctAnswers++;
                         Console.WriteLine("----------- Correct! -----------");
@@ -62,9 +115,11 @@ while (!String.IsNullOrEmpty(line = Console.ReadLine()))
         {
             Console.WriteLine("\n----------- Sorry, you lost -----------");
         }
-    }
 
-    game++;
+        gamesHistory.Add($"Game {game}: {correctAnswers} / {numberQuestions}");
+        game++;
+
+    }
 
     Console.WriteLine(welcomeLine);
     Console.WriteLine(menuOptions);
